652a972e3a341aaf7555ae0c2d7993b28c7f1a51
Nicholas Sofroniew <sofroniewn@gmail.com>
2023-01-12 17:16:41 -0800

    add scvi

---
+++ scripts/train_gene.py
@@ -0,0 +1,33 @@
+from pytorch_lightning import Trainer
+from pytorch_lightning.callbacks import ModelCheckpoint
+from rosa import GeneAnnDataModule, GeneEmbedding2ExpressionModel
+
+
+BASE_PT = "/Users/nsofroniew/Documents/data/multiomics/enformer"
+TABULA_SAPIENS_BY_CELL_TYPE_WITH_EMBEDS_PT = "/Users/nsofroniew/Documents/data/multiomics/cell_census/tabula_sapiens_by_features_with_embeds_prot.h5ad"
+# CKPT_PT = '/Users/nsofroniew/Documents/data/multiomics/enformer/embedding2expression_r2/lightning_logs/version_7/checkpoints/epoch=11958-step=7211277.ckpt'
+CKPT_PT = None
+HEAD = "MLP"
+
+dm = GeneAnnDataModule(TABULA_SAPIENS_BY_CELL_TYPE_WITH_EMBEDS_PT)
+dm.setup()
+
+model = GeneEmbedding2ExpressionModel(in_dim=dm.n_input, out_dim=dm.n_output, head=HEAD)
+print(model)
+
+checkpoint_callback = ModelCheckpoint(
+    save_top_k=2, monitor="val_loss", mode="min", save_last=True
+)
+trainer = Trainer(
+    max_epochs=500_000,
+    check_val_every_n_epoch=1,
+    # log_every_n_steps=10_000,
+    default_root_dir=BASE_PT + f"/Embedding2ExpressionModel",
+    resume_from_checkpoint=CKPT_PT,
+    accelerator="cpu",
+    devices=1,
+    callbacks=[checkpoint_callback],
+    gradient_clip_val=10,
+)
+trainer.fit(model, dm)
+# tensorboard --logdir=lightning_logs/
---
+++ scripts/train_scvi.py
@@ -0,0 +1,23 @@
+import anndata as ad
+from scvi.model import SCVI
+
+
+TABULA_SAPIENS_BY_CELL_TYPE_WITH_EMBEDS_PT = "/Users/nsofroniew/Documents/data/multiomics/cell_census/tabula_sapiens_by_features_with_embeds_prot.h5ad"
+TABULA_SAPIENS_BY_CELL_TYPE_SCVI_MODEL = "/Users/nsofroniew/Documents/data/multiomics/cell_census/tabula_sapiens_by_features_scvi_model_test/"
+
+adata = ad.read_h5ad(TABULA_SAPIENS_BY_CELL_TYPE_WITH_EMBEDS_PT)
+
+train_cells = adata.obs["train"]
+adata_train = adata[train_cells].copy()
+
+SCVI.setup_anndata(adata_train, layer="counts")
+
+# model = SCVI(adata_train, n_layers=4, n_hidden=512, n_latent=64)
+# model = SCVI(adata_train, n_layers=2, n_hidden=256, n_latent=32, dispersion='gene-cell')
+model = SCVI(adata_train, n_layers=2, n_hidden=256, n_latent=32, dispersion='gene', gene_likelihood='nb')
+# model = SCVI(adata_train, n_layers=2, n_hidden=64, n_latent=16)
+# model = SCVI(adata_train)
+
+model.train()
+
+model.save(TABULA_SAPIENS_BY_CELL_TYPE_SCVI_MODEL, overwrite=True)
