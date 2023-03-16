76964186edecba86e2b32c8c626f0ceb97ec222e
sofroniewn <sofroniewn@gmail.com>
2023-03-16 23:32:21 +0000

    minor

--- rosa/modeling/__init__.py
+++ rosa/modeling/__init__.py
@@ -4,5 +4,6 @@ from pytorch_lightning.utilities.rank_zero import LightningDeprecationWarning
 
 from .predict import predict
 from .train import train
+from .modules import RosaLightningModule
 
 warnings.simplefilter(action="ignore", category=LightningDeprecationWarning)
