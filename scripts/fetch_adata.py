90cae866bcd9edfc182d113eec744a39d0cc5531
sofroniewn <sofroniewn@gmail.com>
2023-05-17 02:56:43 +0000

    mouse

--- scripts/fetch_adata.py
+++ scripts/fetch_adata.py
@@ -1,22 +1,26 @@
-import cell_census
+import cellxgene_census
 from pathlib import Path
 
 
 BASE = "/home/ec2-user/cell_census"  # "/Users/nsofroniew/Documents/data/multiomics/cell_census"
-DATASET = "tabula_sapiens"
+DATASET = "tabula_sapiens_2"
+
+# dataset_id = "48b37086-25f7-4ecd-be66-f5bb378e3aea" # tabula muris all
+# organism = "mus_musculus"
+
+dataset_id = "e5f58829-1a66-40b5-a624-9046778e74f5" # tabula sapiens all
+organism = "homo_sapiens"
 
 path = Path(BASE) / (DATASET + ".h5ad")
 
 # Open cell census
-census = cell_census.open_soma()
+census = cellxgene_census.open_soma()
 
 # Fetch desired data and put into an anndata object
-adata = cell_census.get_anndata(
-    census,
-    "Homo sapiens",
-    obs_query={
-        "dataset_id": "e5f58829-1a66-40b5-a624-9046778e74f5",  # tabula sapiens all
-    },
+adata = cellxgene_census.get_anndata(
+    census=census,
+    organism=organism,
+    obs_value_filter=f"dataset_id == '{dataset_id}'",
 )
 print(adata)
 
--- scripts/get_sequences.py
+++ scripts/get_sequences.py
@@ -3,27 +3,31 @@ from rosa.data.sequences import get_all_sequences, get_all_intervals
 import pyfaidx
 
 
-# https://ftp.ensembl.org/pub/release-77/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.toplevel.fa.gz
-FASTA_PT = "/Users/nsofroniew/Documents/data/multiomics/enformer/Homo_sapiens.GRCh38.dna.toplevel.fa"
-GENE_INTERVALS_PT = (
-    "/Users/nsofroniew/Documents/data/multiomics/enformer/Homo_sapiens.GRCh38.genes.bed"
-)
+BASE_PTH = "/home/ec2-user/enformer/"
 SEQUENCE_LENGTH = 196_608
 INTERVAL_ONLY = True
+RELEASE_NUMBER = 109
+SPECIES = 'Mouse'
+# FILE_NAME = 'Homo_sapiens.GRCh38.genes.bed'
+FILE_NAME = 'Mus_musculus.genes.bed'
+
 
 # Open genome
-genome = EnsemblRelease(77)
+genome = EnsemblRelease(RELEASE_NUMBER, species=SPECIES)
 
-if INTERVAL_ONLY:
-    # Get interval around TSS for all genes in gemone
-    intervals = get_all_intervals(genome, SEQUENCE_LENGTH)
+# Get interval around TSS for all genes in gemone
+intervals = get_all_intervals(genome, SEQUENCE_LENGTH)
 
-    # Save interval to BED file
-    intervals.to_csv(GENE_INTERVALS_PT, sep="\t", header=False, index=False)
-else:
-    # Open dna
-    pyfaidx.Faidx(FASTA_PT)
-    dna = pyfaidx.Fasta(FASTA_PT)
+# Save interval to BED file
+intervals.to_csv(BASE_PTH + FILE_NAME, sep="\t", header=False, index=False)
 
-    # Get sequences around TSS for all genes in gemone
-    sequences = get_all_sequences(genome, dna, SEQUENCE_LENGTH)
+
+# # https://ftp.ensembl.org/pub/release-77/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.toplevel.fa.gz
+# FASTA_PT = "/Users/nsofroniew/Documents/data/multiomics/enformer/Homo_sapiens.GRCh38.dna.toplevel.fa"
+
+# # Open dna
+# pyfaidx.Faidx(FASTA_PT)
+# dna = pyfaidx.Fasta(FASTA_PT)
+
+# # Get sequences around TSS for all genes in gemone
+# sequences = get_all_sequences(genome, dna, SEQUENCE_LENGTH)
